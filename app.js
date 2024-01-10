0f6d6be384bc30254d3d12dd6b40357318fc3d1f
Zishan Shaikh <100702557+zishan29@users.noreply.github.com>
2024-01-10 11:53:54 +0530

    Remove a dependency

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 
 const express = require('express');
-const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
@@ -25,7 +24,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors(corsOptions));
 
 app.use('/', indexRouter);
