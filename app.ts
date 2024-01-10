6bbdbf5689625620147a0c5f1bc5de09ccfc71d6
Zishan Shaikh <100702557+zishan29@users.noreply.github.com>
2024-01-10 10:24:23 +0530

    Add mongo connection and cors options

--- app.ts
+++ app.ts
@@ -2,16 +2,40 @@ import express, { type Express } from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
+import mongoose from 'mongoose';
+import cors from 'cors';
 
 import indexRouter from './routes/index';
 
 const app: Express = express();
+
+const mongoDb: string | undefined = process.env.MONGODB_URI;
+
+main().catch((err) => {
+  console.log(err);
+});
+async function main(): Promise<void> {
+  if (typeof mongoDb === 'string') {
+    await mongoose.connect(mongoDb);
+  }
+}
+
+interface corsInterface {
+  origin: string[];
+  optionsSuccessStatus: number;
+}
+
+const corsOptions: corsInterface = {
+  origin: ['https://blog-bice-tau-13.vercel.app', 'http://localhost:3000'],
+  optionsSuccessStatus: 200,
+};
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors(corsOptions));
 
 app.use('/', indexRouter);
 
