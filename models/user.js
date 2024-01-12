0112f2dbddf8364cebb3f7a19506d48e357b6260
Zishan Shaikh <100702557+zishan29@users.noreply.github.com>
2024-01-12 14:05:26 +0530

    Change user model

--- models/user.js
+++ models/user.js
@@ -4,7 +4,8 @@ const { Schema } = mongoose;
 
 const UserSchema = new Schema({
   name: { type: String, required: true },
-  score: { type: String, required: true },
+  time: { type: String, required: true },
+  timeStamp: { type: Date, default: Date.now, required: true },
 });
 
 module.exports = mongoose.model('User', UserSchema);
